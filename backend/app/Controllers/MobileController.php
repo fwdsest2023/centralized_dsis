2e6d5c9de86599379c133ebe33e8fcd0d67b13d4
jommelcabiles <jommelcabiles.dev@gmail.com>
2023-06-26 01:04:57 +0800

    update Android

--- backend/app/Controllers/MobileController.php
+++ backend/app/Controllers/MobileController.php
@@ -11,83 +11,73 @@ class MobileController extends BaseController
     }
 
     public function syncAgentData(){
-        // Check Auth header bearer
-        // $authorization = $this->request->getServer('HTTP_AUTHORIZATION');
-        // if(!$authorization){
-        //     $response = [
-        //         'message' => 'Unauthorized Access'
-        //     ];
+        try {
+            // get the data
+            $payload = $this->request->getJSON();
+            $dataList = $payload->listData;
+            $clientList = [];
+            $bookingList = [];
 
-        //     return $this->response
-        //             ->setStatusCode(401)
-        //             ->setContentType('application/json')
-        //             ->setBody(json_encode($response));
-        //     exit();
-        // }
+            foreach ($dataList as $key => $value) {
+                $clientList[$key] = [
+                    "storeName" => $value->client->storeName,
+                    "address" => $value->client->address,
+                    "branch" => $value->client->branch,
+                    "regionId" => $value->client->regionId,
+                    "categoryId" => $value->client->categoryId,
+                    "geoLocation" => $value->client->geoLocation,
+                    "type" => $value->client->type,
+                    "contactPerson" => $value->client->contactPerson,
+                    "attendance" => $value->attendance,
+                    "remarks" => $value->remarks,
+                    "files" => $value->files,
+                ];
 
-        // get the data
-        $payload = $this->request->getJSON();
-        $dataList = $payload->listData;
-        $clientList = [];
-        $bookingList = [];
+                $bookingList[$key] = [
+                    "client" => $clientList[$key],
+                    "booking" => $value->booking
+                ];
+            }
 
-        foreach ($dataList as $key => $value) {
-            $clientList[$key] = [
-                "storeName" => $value->client->storeName,
-                "address" => $value->client->address,
-                "branch" => $value->client->branch,
-                "regionId" => $value->client->regionId,
-                "categoryId" => $value->client->categoryId,
-                "geoLocation" => $value->client->geoLocation,
-                "type" => $value->client->type,
-                "contactPerson" => $value->client->contactPerson,
-                "attendance" => $value->attendance,
-                "remarks" => $value->remarks,
-                "files" => $value->files,
+
+            $data = [
+                "agentId" => $payload->agentId,
+                "category" => $payload->category,
+                "client" => json_encode($clientList),
+                "booking" => json_encode($bookingList)
             ];
-
-            $bookingList[$key] = [
-                "client" => $clientList[$key],
-                "booking" => $value->booking
-            ];
-        }
-
-
-        $data = [
-            "agentId" => $payload->agentId,
-            "category" => $payload->category,
-            "client" => json_encode($clientList),
-            "booking" => json_encode($bookingList)
-        ];
-        
-        // echo '<pre>';
-        // print_r($data);
-        // exit();
-        $query = $this->mobModel->insert($data);
-
-        if($query){
-
-            $response = [
-                'title' => 'Sync Complete',
-                'message' => 'data hasbeen successfully sync to database'
-            ];
- 
-            return $this->response
-                    ->setStatusCode(200)
-                    ->setContentType('application/json')
-                    ->setBody(json_encode($response));
             
-        } else {
-            $response = [
-                'title' => 'Sync Failed!',
-                'message' => 'Something is went wrong',
-                'error' => $query
-            ];
- 
-            return $this->response
-                    ->setStatusCode(400)
-                    ->setContentType('application/json')
-                    ->setBody(json_encode($response));
+            // echo '<pre>';
+            // print_r($data);
+            // exit();
+            $query = $this->mobModel->insert($data);
+
+            if($query){
+
+                $response = [
+                    'title' => 'Sync Complete',
+                    'message' => 'data hasbeen successfully sync to database'
+                ];
+    
+                return $this->response
+                        ->setStatusCode(200)
+                        ->setContentType('application/json')
+                        ->setBody(json_encode($response));
+                
+            } else {
+                $response = [
+                    'title' => 'Sync Failed!',
+                    'message' => 'Something is went wrong',
+                    'error' => $query
+                ];
+    
+                return $this->response
+                        ->setStatusCode(400)
+                        ->setContentType('application/json')
+                        ->setBody(json_encode($response));
+            }
+        } catch (\Throwable $th) {
+            throw $th;
         }
     } 
 
